36be2849a8b982227454513622f8ca380e08a0e7
Кгыдфт <ruslanisin00@gmail.com>
2026-03-11 11:59:27 +0500

    fix_checkout

--- cypress/e2e/Regress_Test/checkout.cy.js
+++ cypress/e2e/Regress_Test/checkout.cy.js
@@ -113,7 +113,53 @@ describe('Оформление заказа', () => {
 
         cy.visit('/basket')
 
-        cy.url().should('include', 'basket')
+        cy.url({
+            timeout: 20000
+        }).should('include', 'basket')
+
+        cy.contains('span', 'Оформить заказ', {
+                timeout: 20000
+            })
+            .first()
+            .should('be.visible')
+            .click()
+
+        cy.url({
+            timeout: 20000
+        }).should('include', '/checkout')
+
+    })
+
+    it('Переход в оформление с кнопки "Купить сейчас"', () => {
+
+        cy.visit('/')
+
+        Checkout.auth_checkout()
+
+        cy.contains('a', 'Смартфоны Apple', {
+                timeout: 20000
+            }).first()
+            .click({
+                timeout: 20000
+            })
+
+        cy.intercept('GET', '**/api/v3/product/*').as('getProduct');
+
+        cy.get('div.rounded-lg.rounded-mi-l > div.p-4.sm\\:p-6:nth-of-type(2) > div.w-full.h-full > div.relative.flex > a.w-full.justify-center:nth-of-type(2)', {
+                timeout: 20000
+            }).eq(0)
+            .click()
+
+        Add_basket.getProduct()
+
+        Add_basket.intercept_request()
+
+        cy.contains('button', 'Купить сейчас', {
+                timeout: 20000
+            }).first()
+            .click()
+
+        cy.url().should('include', '/checkout')
 
     })
 })
--- cypress/e2e/Regress_Test/test.cy.js
+++ cypress/e2e/Regress_Test/test.cy.js
@@ -1,6 +1,10 @@
 import actionPage from '../../integration/pageObjects/action';
+import add_basket from '../../integration/pageObjects/add_basket';
+import checkout from "../../integration/pageObjects/checkout";
 
 const ActionPage = new actionPage();
+const Add_basket = new add_basket();
+const Checkout = new checkout();
 
 describe('Тестовый файл', () => {
     beforeEach(() => {
@@ -10,11 +14,31 @@ describe('Тестовый файл', () => {
     });
 
     it('Сравнение категорий из API и UI через intercept', () => {
-        // 1. Перехватываем запрос (подставь свой URL)
-        cy.intercept('GET', '/api/v3/promotions').as('promotions');
+        cy.visit('/')
 
-        cy.visit('/useful/shares/');
+        Checkout.auth_checkout()
 
-        ActionPage.check_action_title();
+        cy.contains('a', 'Смартфоны Apple', {
+                timeout: 20000
+            }).first()
+            .click({timeout: 20000})
+
+        cy.intercept('GET', '**/api/v3/product/*').as('getProduct');
+
+        cy.get('div.rounded-lg.rounded-mi-l > div.p-4.sm\\:p-6:nth-of-type(2) > div.w-full.h-full > div.relative.flex > a.w-full.justify-center:nth-of-type(2)', {
+                timeout: 20000
+            }).eq(0)
+            .click()
+
+        Add_basket.getProduct()
+
+        Add_basket.intercept_request()
+
+        cy.contains('button', 'Купить сейчас', {
+                timeout: 20000
+            }).first()
+            .click()
+
+        cy.url().should('include', '/checkout')
     });
 })
